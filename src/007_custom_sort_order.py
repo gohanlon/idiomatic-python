dc796f26b9e31da3741012e0eef869c35a8ebecb
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-05 11:11:53 -0700

    idiomatic custom sort order

--- src/007_custom_sort_order.py
+++ src/007_custom_sort_order.py
@@ -1,9 +1,3 @@
 colors = ['red', 'green', 'blue', 'yellow']
 
-def compare_length(c1, c2):
-    if len(c1) < len(c2): return -1
-    if len(c1) > len(c2): return 1
-    return 0
-
-# Breaks on Python 3 because comparator functions are replaced with key functions
-#print(sorted(colors, cmp=compare_length))
+print(sorted(colors, key=len))
