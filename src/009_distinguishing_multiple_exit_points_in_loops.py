41bd8f0457854dd698beb0b64bcb4ade3f184ac3
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-05 12:08:17 -0700

    non-idiomatic distinguishing multiple exit points in loops

---
+++ src/009_distinguishing_multiple_exit_points_in_loops.py
@@ -0,0 +1,11 @@
+def find(seq, target):
+    found = False
+    for i, value in enumerate(seq):
+        if value == target:
+            found = True
+            break
+    if not found:
+        return -1
+    return i
+
+print(find([1,2,3], 4))
