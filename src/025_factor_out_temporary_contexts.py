e8e3b037e661bfd2f6221ef8ccc3ca602b7680a6
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-06 16:55:28 -0700

    idiomatic use of temporary context

--- src/025_factor_out_temporary_contexts.py
+++ src/025_factor_out_temporary_contexts.py
@@ -2,9 +2,7 @@ import decimal
 
 print(decimal.Decimal(355) / decimal.Decimal(113))
 
-old_context = decimal.getcontext().copy()
-decimal.getcontext().prec = 50
-print(decimal.Decimal(355) / decimal.Decimal(113))
-decimal.setcontext(old_context)
+with decimal.localcontext(decimal.Context(prec=50)):
+    print(decimal.Decimal(355) / decimal.Decimal(113))
 
 print(decimal.Decimal(355) / decimal.Decimal(113))
