eec22d3063a97ea0dabd5c8674bad8b40406b3f0
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-06 13:33:06 -0700

    idiomatic concatenating strings

--- src/022_concatenating_strings.py
+++ src/022_concatenating_strings.py
@@ -1,6 +1,3 @@
 names = ['raymond', 'rachel', 'matthew', 'roger', 'betty', 'melissa', 'judith', 'charlie']
 
-s = names[0]
-for name in names[1:]:
-    s += ', ' + name
-print(s)
+print(', '.join(names))
