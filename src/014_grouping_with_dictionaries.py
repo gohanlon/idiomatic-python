1683c546a1b9d1a81af0cff7baf0fc7aa54540df
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-05 17:08:36 -0700

    use defaultdict to group with dictionaries (most idiomatic)

--- src/014_grouping_with_dictionaries.py
+++ src/014_grouping_with_dictionaries.py
@@ -1,8 +1,10 @@
+from collections import defaultdict
+
 names = ['raymond', 'rachel', 'matthew', 'roger', 'betty', 'melisa', 'judith', 'charlie']
 
-d = {}
+d = defaultdict(list)
 for name in names:
     key = len(name)
-    d.setdefault(key, []).append(name)
+    d[key].append(name)
 
 print(d)
