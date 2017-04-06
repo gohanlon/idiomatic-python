067878bf910fc3b0d624747b6928a358ed31b26c
Galen O’Hanlon <gohanlon@icloud.com>
2017-04-06 14:28:59 -0700

    more idiomatic decorator as this use is built in as lru_cache

--- src/024_using_decorators_to_factor_out_admin_logic.py
+++ src/024_using_decorators_to_factor_out_admin_logic.py
@@ -1,6 +1,7 @@
-from functools import wraps
+from functools import wraps, lru_cache
 
 import urllib.request
+import time
 
 def cache(func):
     saved = {}
@@ -15,8 +16,22 @@ def cache(func):
 
 @cache
 def web_lookup(url):
+    time.sleep(3)
     return urllib.request.urlopen(url).read()
 
+@lru_cache(maxsize=None)
+def web_lookup2(url):
+    time.sleep(3)
+    return urllib.request.urlopen(url).read()
+
+print("Custom decorator:")
 print(web_lookup('http://example.com'))
 print('\n')
 print(web_lookup('http://example.com'))
+
+print('\n' * 2)
+
+print('Built-in lru_cache decorator:')
+print(web_lookup2('http://example.com'))
+print('\n')
+print(web_lookup2('http://example.com'))
